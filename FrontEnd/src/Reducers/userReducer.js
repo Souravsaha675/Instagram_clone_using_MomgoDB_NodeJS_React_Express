5cc3b27efdc2b586b7d51a352fc120d3ef45bd14
sourav saha <souravsaha675@gmail.com>
2020-07-17 23:08:27 +0530

    frontsite commit

---
+++ FrontEnd/src/Reducers/userReducer.js
@@ -0,0 +1,18 @@
+export const initialState=null;
+
+
+
+export const reducer=(state,action)=>{
+    if(action.type==="USER"){
+        return action.payload
+    } else if(action.type==="CLEAR"){
+        return  null;
+    } else if(action.type==="UPDATE"){
+        return {
+            ...state,
+            followers:action.payload.followers,
+            following:action.payload.following
+        }
+    }
+    return state
+}
