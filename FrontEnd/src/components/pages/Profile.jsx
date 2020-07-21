6c8a1f8b1118f62d347e17b31fca5c45895395e1
sourav saha <souravsaha675@gmail.com>
2020-07-21 20:48:12 +0530

    Profile chenges

--- FrontEnd/src/components/pages/Profile.jsx
+++ FrontEnd/src/components/pages/Profile.jsx
@@ -1,6 +1,6 @@
 import React, { useEffect, useState, useContext } from "react";
 import { userContext } from "../../App";
-
+import {Clouduri} from "../../CloudKey"
 function Profile(){
     const[images,setimages]=useState([]);
     const {state,dispatch}=useContext(userContext)
@@ -15,6 +15,25 @@ function Profile(){
             setimages(result.myposts)
         })
     },[])
+
+    /*function updatePic(){
+        const data = new FormData()
+        data.append("file", image)
+        data.append("upload_preset", "InstaBook")
+        data.append("cloud_name", "sourav675")
+        fetch(Clouduri, {
+                method: "post",
+                body: data
+            })
+            .then(res => res.json())
+            .then(data => {
+                seturl(data.url)
+            })
+            .catch(err => {
+                console.log(err)
+            })
+    }*/
+
     return(
         <>
         {state?
@@ -27,16 +46,23 @@ function Profile(){
                 borderBottom:"1px solid grey"
             }}>
                 <div>
-                    <img alt="" style={{width:"160px",height:"160px",borderRadius:"80px"}} src = "https://images.unsplash.com/photo-1542611436-4d894c88d240?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=948&q=80"></img>
+                    <img alt="" style={{width:"160px",height:"160px",borderRadius:"80px"}} src ={state?state.profileImg:"lodding"} ></img>
+                    <div style={{paddingLeft:"25px",paddingTop:"10px"}}>
+                    <button className="btn waves-effect waves-light">
+                        Update Img
+                    </button>
+                </div>
                 </div>
                 <div style={{marginLeft:"15px"}}>
                     <h4 style={{fontFamily:"Courier New, Courier, monospace"}}>{state?state.name:"lodding"}</h4>
+                    <h6 style={{fontFamily:"Courier New, Courier, monospace"}}>{state?state.email:"lodding"}</h6>
                     <div style={{display:"flex",justifyContent:"space-between",width:"110%"}}>
                         <h6>{images.length} Posts </h6>
                         <h6>{state?state.followers.length:"0"} Followers </h6>
                         <h6>{state?state.following.length:"0"} Following</h6>
                     </div>
                 </div>
+                
             </div>
             <div className="profileposts">
                 {
